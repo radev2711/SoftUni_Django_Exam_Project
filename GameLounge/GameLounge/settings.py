15f67262cf0c35eefac8586b8fa269b838a53b33
Ivo Radev <45425202+radev2711@users.noreply.github.com>
2023-08-11 09:09:44 +0300

    delete DB credentials

--- GameLounge/GameLounge/settings.py
+++ GameLounge/GameLounge/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = "GameLounge.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "loungeDB",
-        "USER": "postgres-user",
-        "PASSWORD": "password",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
